77ec033351223aa0ff274e2e3e5314dcb86e22c8
Dorian Buck <[dorianbuck@gmail.com]>
2021-09-06 21:03:46 +0200

    adds the header, footer and hello component. Also semantic-ui styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Hello from "./Hello";
+import Header from "./Header";
+import Footer from "./Footer";
 
 function App() {
-  return <h1 className="App">Hello World!</h1>;
+  return (
+    <>
+      <Header />
+      <Hello />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
