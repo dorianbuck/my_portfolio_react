1bea2577eb7275e5df119c6226c3fcc0dd3c17f5
Dorian Buck <[dorianbuck@gmail.com]>
2021-09-07 12:54:42 +0200

    adds test for components - about, my projects. Also content i app.jsx for routing

---
+++ src/About.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Container } from "semantic-ui-react";
+
+const About = () => {
+  return (
+    <Container>
+      <h1 id="about-header">About Me</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est, fugiat!
+        Reprehenderit rerum incidunt quae libero sit velit dolorum iste at
+        necessitatibus dolor, beatae ad, reiciendis, veritatis consequuntur
+        harum. Recusandae, fugit!
+      </p>
+    </Container>
+  );
+};
+
+export default About;
---
+++ src/Projects.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Container } from "semantic-ui-react";
+
+const Projects = () => {
+  return (
+    <Container>
+      <h1 id="projects-header">My Projects</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde doloribus
+        magni at neque adipisci vitae tempora debitis animi minima quae magnam
+        sint ab quod incidunt aliquam, laboriosam accusamus laudantium officia?
+      </p>
+    </Container>
+  );
+};
+
+export default Projects;
