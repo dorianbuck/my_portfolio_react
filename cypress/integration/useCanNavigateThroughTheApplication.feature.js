59630f6dd2ccd12af1160a0146c905b2618271f9
Dorian Buck <[dorianbuck@gmail.com]>
2021-09-09 11:41:24 +0200

    adds useEffect hook to replace class Component

--- cypress/integration/useCanNavigateThroughTheApplication.feature.js
+++ cypress/integration/useCanNavigateThroughTheApplication.feature.js
@@ -66,7 +66,7 @@ describe("User can navigate the app", () => {
       cy.get("#header").click();
     });
     it("displays Hello World", () => {
-      cy.get("#hello").should("contain", "Hello");
+      cy.get("#hello").should("contain", "Welcome");
     });
     it("displays correct url", () => {
       cy.url().should("not.contain", "projects").and("not.contain", "about");
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,26 @@
-import React from "react";
-import { Menu, Segment } from "semantic-ui-react";
+import React, { Component } from "react";
+import { Menu } from "semantic-ui-react";
 import { NavLink, Link } from "react-router-dom";
 
-const Header = () => {
-  return (
-    <Segment inverted>
-      <Menu inverted secondary>
+export default class MenuTabularOnTop extends Component {
+  state = { activeItem: "my portfolio" };
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+
+  render() {
+    
+    const { activeItem } = this.state;
+    return (
+      <Menu attached="top" tabular>
         <Menu.Item
           id="header"
           name="my portfolio"
           as={Link}
           to={{ pathname: "/" }}
           icon="briefcase"
+          active={activeItem === "my portfolio"}
+          onClick={this.handleItemClick}
+          
         />
         <Menu.Item
           position="right"
@@ -19,22 +28,28 @@ const Header = () => {
           name="about me"
           as={NavLink}
           to={{ pathname: "/about" }}
+          active={activeItem === "about me"}
+          onClick={this.handleItemClick}
         />
         <Menu.Item
           id="projects-tab"
           name="my projects"
           as={NavLink}
           to={{ pathname: "/projects" }}
+          active={activeItem === "my projects"}
+          onClick={this.handleItemClick}
         />
         <Menu.Item
           id="contact-tab"
           name="Contact"
           as={NavLink}
           to={{ pathname: "/contact" }}
+          active={activeItem === "contact"}
+          onClick={this.handleItemClick}
         />
       </Menu>
-    </Segment>
-  );
-};
+    );
+  }
+}
 
-export default Header;
+// export default Header;
